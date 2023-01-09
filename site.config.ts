330a07a12567aadbdb4c7c2033145198b9da70c7
blackmax <blackmax1886@gmail.com>
2023-01-09 16:31:57 +0900

    Refactor : rename domain & site

--- site.config.ts
+++ site.config.ts
@@ -9,8 +9,8 @@ export default siteConfig({
   rootNotionSpaceId: null,
 
   // basic site info (required)
-  name: 'Next.js Notion Starter Kit',
-  domain: 'nextjs-notion-starter-kit.blackmax1886',
+  name: 'blackmax tech blog',
+  domain: 'blackmax-sixevo-blog',
   author: 'blackmax1886',
 
   // open graph metadata (optional)
